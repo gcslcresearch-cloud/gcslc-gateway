227ffcac5e622b7ce5d0ea2f88e516dd85049ae4
gcslcresearch-cloud <gcslc.research@gmail.com>
2026-04-17 14:32:02 +0100

    Expand institutional matrix to V34 opportunity radar.
    Injects SMEDAN/ITF/NEMA/Federal Ministry of Works into opportunity logic, upgrades each state pop-up to a Gold/Cyan strategic radar chart, adds the departure bridge header, and reinforces proprietary watermark visibility over Vision and Governance sidebars while hard-saving the root index build.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (() => {
   const root = window;
   const defaults = {
-    version: "SOVEREIGN_TOTAL_SYNC_V33",
+    version: "INSTITUTIONAL_MATRIX_V34_EXPANSION",
     theme: {
       font: "Goldman",
       background: "#001a33",
@@ -9,13 +9,15 @@
     senderWhitelist: ["GALADIMA_R"],
     institutionalTicker: ["CBN", "NNPCL", "RMAFC", "NSA", "NIMASA", "NAHCON"],
     institutionalTickerSecondary: ["AMCON", "NDIC", "NHIS", "Hajj Commission", "INEC"],
+    opportunityNodes: ["Federal Ministry of Works", "SMEDAN", "ITF", "NEMA"],
     alertTriggerNodes: ["Police", "Immigration"],
     sovereignKeys: ["LW15954", "GCSLC2026", "SOVEREIGN_MANDATE"],
     kadunaPopulation: 2000000,
     kadunaLgaCount: 23,
   };
 
-  root.GCSLC_APP_V33 = Object.assign({}, defaults, root.GCSLC_APP_V33 || {});
-  root.GCSLC_APP_V32 = root.GCSLC_APP_V33;
-  root.GCSLC_APP_V30 = root.GCSLC_APP_V33;
+  root.GCSLC_APP_V34 = Object.assign({}, defaults, root.GCSLC_APP_V34 || {});
+  root.GCSLC_APP_V33 = root.GCSLC_APP_V34;
+  root.GCSLC_APP_V32 = root.GCSLC_APP_V34;
+  root.GCSLC_APP_V30 = root.GCSLC_APP_V34;
 })();
